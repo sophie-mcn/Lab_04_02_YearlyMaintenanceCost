d54189bc6a8c6230a115c972b1141423a076d5e5
sophie-mcn <mcnamask@mail.uc.edu>
2025-09-18 13:20:24 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    double springCost = 100;
+    double summerCost = 200;
+    double autumnCost = 300;
+    double winterCost = 400;
+
+    double totalYearlyCost =  springCost + summerCost + autumnCost + winterCost;
+        System.out.println("Total yearly cost: " + totalYearlyCost);
+
+    }
+}
